95b2df70fd5f357aa1b5a5f02999ce921843978d
ivander27 <timotius.ivander04@gmail.com>
2022-05-14 16:00:22 +0700

    update activity_profile, updatepassword, updateprofile

--- app/src/main/java/com/example/noveltoon/profile.java
+++ app/src/main/java/com/example/noveltoon/profile.java
@@ -2,13 +2,26 @@ package com.example.noveltoon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class profile extends AppCompatActivity {
-
+    private Button btnupdateprofile;
+    private Button btnupdatepassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        btnupdateprofile = findViewById(R.id.button_Updateprofil);
+        btnupdatepassword = findViewById(R.id.button_Updatepassword);
+
+        btnupdateprofile.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), updateprofile.class));
+        });
+        btnupdatepassword.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), updatepassword.class));
+        });
     }
 }
