43dcfcd14514b03a1ae667aaf467f4c0162ffa7c
Jestr13 <johanesvictor46@gmail.com>
2022-05-18 18:00:49 +0700

    progress ya ges ya

--- app/src/main/java/com/example/noveltoon/MainActivity.java
+++ app/src/main/java/com/example/noveltoon/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.noveltoon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -12,6 +14,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.noveltoon.databinding.ActivityMainBinding;
 import com.example.noveltoon.databinding.ActivityNavigationBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,17 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
         btnNextPage = findViewById(R.id.button_nextpage);
 
-        btnNextPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.welcome, new HomeFragment()).commit();
-            }
-        });
-
-//        btnNextPage.setOnClickListener(v -> {
-//            startActivity(new Intent(getApplicationContext(), signin.class)); //sign in
+//        btnNextPage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+//                fragmentTransaction.replace(R.id.welcome, new Home()).commit();
+//            }
 //        });
+
+        btnNextPage.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), signin.class));
+        });
     }
 
 
--- app/src/main/java/com/example/noveltoon/updateprofile.java
+++ app/src/main/java/com/example/noveltoon/updateprofile.java
@@ -1,10 +1,14 @@
 package com.example.noveltoon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class updateprofile extends AppCompatActivity {
     private Button btnsaveprofile;
@@ -20,6 +24,30 @@ public class updateprofile extends AppCompatActivity {
 
         btnbackprofile.setOnClickListener(v -> {
             startActivity(new Intent(getApplicationContext(), profile.class));
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), profile.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), Home.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.logout:
+                        finish();
+//                        startActivity(new Intent(getApplicationContext(), signin.class));
+//                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
         });
     }
 }
